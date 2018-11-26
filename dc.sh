6d075571cd18f1d3b9c42860c8c56f732251757d
Shahidh K Muhammed <shahidh@hasura.io>
2018-11-26 15:03:14 +0530

    add realtime chat

--- dc.sh
+++ dc.sh
@@ -3,6 +3,7 @@
 docker-compose \
   -f docker-compose.yaml \
   -f realtime-poll.yaml \
+  -f realtime-chat.yaml \
   -f realtime-location-tracking.yaml \
   -f react-apollo-todo.yaml \
   -f serverless-push.yaml \
